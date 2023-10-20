e0454568ad4a6000a535c9bf23684bb1dfee7b15
RockSkater99 <daniel_maya99@protonmail.com>
2023-10-20 16:42:47 -0600

    Update

--- gerente.php
+++ gerente.php
@@ -12,7 +12,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Inicio de Sesion</title>
+	<title>Inicio de Sesio</title>
 
 	<link href="img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
 	<link href="img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
